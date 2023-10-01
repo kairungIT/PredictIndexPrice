d0421ac11e9ec772bf3c23a81e18c8e4ad5e7682
kairungIT <113651426+kairungIT@users.noreply.github.com>
2023-10-01 10:26:10 +0700

    .commit

--- MyApp1.py
+++ MyApp1.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import streamlit as st
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score,mean_squared_error
 
 st.title("Index Price Prediction")
 st.header("Index Price Prediction from NPRU")
@@ -12,3 +17,19 @@ st.write(df.head(10))
 st.line_chart(
    df, x="interest_rate", y=["unemployment_rate", "stock_index_price"], color=["#FF0000", "#0000FF"]  # Optional
 )
+
+x=df[['interest_rate','unemployment_rate']]
+y=df['stock_index_price']
+pf=PolynomialFeatures(degree=3)
+x_poly=pf.fit_transform(x)
+
+x_train,x_test,y_train,y_test =train_test_split(x_poly,y,random_state=0)
+
+modelRegress=LinearRegression()
+modelRegress.fit(x_train,y_train)
+
+x_input=[[2,5],[2.2,5.7]]
+
+y_predict=modelRegress.predict(pf.fit_transform(x_input))
+
+st.write(y_predict)
